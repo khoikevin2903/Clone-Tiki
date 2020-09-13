b2fd5eef3430a7be101d8fdfeabaf80bec5b991a
Anh Khoi <khoikevin2903@gmail.com>
2020-09-13 20:33:30 +0700

    Add Date for Form Edit

--- src/components/Account/Calendar.js
+++ src/components/Account/Calendar.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 
 
 
 function Calendar(props) {
 
-	const { Arrays } = props;
+	const { Arrays, val, name } = props;
+	
+	const [date, setDate] = useState(val);
 
 	const elm = Arrays.map((arr, index) => {
-		return <option value={index} key={index}>{arr}</option>
+		return <option value={arr} key={index}>{arr}</option>
 	})
+
+	useEffect(() => {
+		setDate(val);
+	}, [val])
+
+	const HandleChange = (event) => {
+		const target = event.target;
+		var value = target.val;
+		setDate(value);
+	}
 
 	return (
 		<div className="inline-block relative w-56 font-thin mr-4 p-2">
-			<select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+			<select className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+				name={name} value={date} onChange={HandleChange}>
 				{elm}
 			</select>
 			<div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
