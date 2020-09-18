cb8fc7b6b1357c68b629f60f312b98e3a42e36eb
Anh Khoi <khoikevin2903@gmail.com>
2020-09-18 08:12:07 +0700

    add file constants address

---
+++ src/constants/address.js
@@ -0,0 +1,11 @@
+export const THANHPHO = [
+    "Chọn Tỉnh/Thành Phố", "Hồ Chí Minh", "Hà Nội", "Đà Nẵng", "An Giang", "Bà Rịa-Vũng Tàu"
+];
+
+export const HUYEN = [
+    "Chọn Quận/Huyện", "Huyện Hoàng Sa", "Quận Thanh Khê", "Huyện Hòa Vang", "Quận Sơn Trà", "Quận Liên Chiểu", "Quận Hải Châu", "Quận Cẩm Lệ", "Quận Ngũ Hành Sơn"
+];
+
+export const XA = [
+    "Chọn Phường/Xã" ,"Xã Hòa Bắc", "Xã Hòa Châu", "Xã Hòa Khương", "Xã Hòa Liên", "Xã Hòa Phong", "Xã Hòa Nhơn", "Xã Hòa Phú", "Xã Hòa Phước", "Hòa Sơn", "Xã Hòa Tiến"
+];
